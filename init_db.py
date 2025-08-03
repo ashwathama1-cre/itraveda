b84db2bd068b531085b18a855b950380d485338e
ashwathama1-cre <shauryag311@gmail.com>
2025-08-03 17:33:24 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,7 +1,7 @@
 # init_db.py
 from app import app, db
 from models import *
-
-with app.app_context():
-    db.create_all()
-    print("✅ Database tables created.")
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("✅ Tables created.")
